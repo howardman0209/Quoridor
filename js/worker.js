35e4db5863e61b2a0da801653fb714bbf86f7332
Howard Man <howard.man@spectratech.com>
2024-03-22 01:14:56 +0800

    mark TODO

--- js/worker.js
+++ js/worker.js
@@ -36,3 +36,10 @@ function aiAction(data) {
 
     callback(`AI`, result);
 }
+
+/*
+TODO: 
+- Fix tree bias problem (expansion -> too much node of block and only a few node of move)
+- add condition to use MCTS or heuristic eg. (when self and opponent no block left -> move with shortest path...)
+
+*/
